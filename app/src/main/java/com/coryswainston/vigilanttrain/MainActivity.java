9ff8393b029b44bd16a8b4a8393bfe51523af3e8
coryswainston <swainston.cory89@gmail.com>
2018-03-20 03:02:33 -0600

    Add Facebook auth

--- app/src/main/java/com/coryswainston/vigilanttrain/MainActivity.java
+++ app/src/main/java/com/coryswainston/vigilanttrain/MainActivity.java
@@ -1,10 +1,23 @@
 package com.coryswainston.vigilanttrain;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,34 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.textView);
         textView.setText("Grandmas");
+
+        callbackManager = CallbackManager.Factory.create();
+
+        final LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i(TAG, "FB Login was cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e(TAG, error.getMessage());
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
